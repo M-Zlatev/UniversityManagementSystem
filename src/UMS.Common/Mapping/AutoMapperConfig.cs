6d709371bcb50ac1c46925c95cfcb4ea8b6dfb6c
Mariyan Zlatev <mariyan.zlatev.bg@gmail.com>
2021-02-02 23:49:00 +0200

    Updated AutoMapper config

--- src/UMS.Common/Mapping/AutoMapperConfig.cs
+++ src/UMS.Common/Mapping/AutoMapperConfig.cs
@@ -25,85 +25,50 @@
 
             initialized = true;
 
-            var types = assemblies.SelectMany(a => a.GetExportedTypes()).ToList();
-
             var config = new MapperConfigurationExpression();
             config.CreateProfile(
                 "ReflectionProfile",
                 configuration =>
                 {
-                    // IMapFrom<>
-                    foreach (var map in GetFromMaps(types))
-                    {
-                        configuration.CreateMap(map.Source, map.Destination);
-                    }
+                    var modelRegistrations = AppDomain
+                      .CurrentDomain
+                      .GetAssemblies()
+                      .Where(a => a.GetName().Name.StartsWith("UMS."))
+                      .SelectMany(a => a.GetExportedTypes())
+                      .Where(t => t.IsClass && !t.IsAbstract)
+                      .Select(t => new
+                      {
+                          Type = t,
+                          MapFrom = GetMappingModel(t, typeof(IMapFrom<>)),
+                          MapTo = GetMappingModel(t, typeof(IMapTo<>)),
+                          ExplicitMap = t.GetInterfaces()
+                              .Where(i => i == typeof(IMapExplicitly))
+                              .Select(i => (IMapExplicitly)Activator.CreateInstance(t))
+                              .FirstOrDefault(),
+                      });
 
-                    // IMapTo<>
-                    foreach (var map in GetToMaps(types))
+                    foreach (var modelRegistration in modelRegistrations)
                     {
-                        configuration.CreateMap(map.Source, map.Destination);
-                    }
+                        if (modelRegistration.MapFrom != null)
+                        {
+                            configuration.CreateMap(modelRegistration.MapFrom, modelRegistration.Type);
+                        }
 
-                    // IMapExplicitly
-                    foreach (var map in GetCustomMappings(types))
-                    {
-                        map.RegisterMappings(configuration);
+                        if (modelRegistration.MapTo != null)
+                        {
+                            configuration.CreateMap(modelRegistration.Type, modelRegistration.MapTo);
+                        }
+
+                        modelRegistration.ExplicitMap?.RegisterMappings(configuration);
                     }
                 });
             MapperInstance = new Mapper(new MapperConfiguration(config));
         }
 
-        private static IEnumerable<TypesMap> GetFromMaps(IEnumerable<Type> types)
-        {
-            var fromMaps = from t in types
-                           from i in t.GetTypeInfo().GetInterfaces()
-                           where i.GetTypeInfo().IsGenericType &&
-                                 i.GetGenericTypeDefinition() == typeof(IMapFrom<>) &&
-                                 !t.GetTypeInfo().IsAbstract &&
-                                 !t.GetTypeInfo().IsInterface
-                           select new TypesMap
-                           {
-                               Source = i.GetTypeInfo().GetGenericArguments()[0],
-                               Destination = t,
-                           };
-
-            return fromMaps;
-        }
-
-        private static IEnumerable<TypesMap> GetToMaps(IEnumerable<Type> types)
-        {
-            var toMaps = from t in types
-                         from i in t.GetTypeInfo().GetInterfaces()
-                         where i.GetTypeInfo().IsGenericType &&
-                               i.GetTypeInfo().GetGenericTypeDefinition() == typeof(IMapTo<>) &&
-                               !t.GetTypeInfo().IsAbstract &&
-                               !t.GetTypeInfo().IsInterface
-                         select new TypesMap
-                         {
-                             Source = t,
-                             Destination = i.GetTypeInfo().GetGenericArguments()[0],
-                         };
-
-            return toMaps;
-        }
-
-        private static IEnumerable<IMapExplicitly> GetCustomMappings(IEnumerable<Type> types)
-        {
-            var customMaps = from t in types
-                             from i in t.GetTypeInfo().GetInterfaces()
-                             where typeof(IMapExplicitly).GetTypeInfo().IsAssignableFrom(t) &&
-                                   !t.GetTypeInfo().IsAbstract &&
-                                   !t.GetTypeInfo().IsInterface
-                             select (IMapExplicitly)Activator.CreateInstance(t);
-
-            return customMaps;
-        }
-
-        private class TypesMap
-        {
-            public Type Source { get; set; }
-
-            public Type Destination { get; set; }
-        }
+        private static Type GetMappingModel(Type type, Type mappingInterface)
+        => type.GetInterfaces()
+            .FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == mappingInterface)
+            ?.GetGenericArguments()
+            .First();
     }
 }
