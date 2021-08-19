2c38dd8290c84afdf4a22b65c3bd57630c4130a5
Mariyan Zlatev <mariyan.zlatev.bg@gmail.com>
2021-08-19 17:36:18 +0300

    Added Tests and Sandbox area

---
+++ src/UMS.Web.Tests/WebTests.cs
@@ -0,0 +1,13 @@
+﻿namespace UMS.Web.Tests
+{
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc.Testing;
+
+    using Xunit;
+
+    public class WebTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+    }
+}
